5e4b0090df035d3d455498843dfaf01b2d49041e
pyhton-delevoper <viktorov.vadimka@gmail.com>
2023-01-17 13:40:44 +0700

    create some variables

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+import psycopg2
+import os
 from dotenv import load_dotenv
+from flask import (Flask,
+                   render_template,
+                   url_for)
 
 
 app = Flask(__name__)
-
 app.secret_key = load_dotenv('SECRET_KEY')
 
+db_url = os.getenv('DATABASE_URL')
+connect = psycopg2.connect(db_url)
 
-@app.route('/')
+
+@app.get('/')
 def start_page():
-    return 'hello'
+    return render_template('start.html')
